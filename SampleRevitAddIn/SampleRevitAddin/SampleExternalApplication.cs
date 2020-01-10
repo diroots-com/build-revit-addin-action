3a2f70eb02548776dbac99927098ae8c402ed56c
Eric Anastas <aireq303@gmail.com>
2020-01-09 22:24:21 -0800

    Created  SampleRevitAddIn projects for Revti 2017 to 2020

---
+++ SampleRevitAddIn/SampleRevitAddin/SampleExternalApplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+
+namespace SampleRevitAddin
+{
+    public class SampleExternalApplication : Autodesk.Revit.UI.IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ SampleRevitAddIn/SampleRevitAddin/VersionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleRevitAddin
+{
+    public class VersionInfo
+    {
+        [assembly: AssemblyTitle("SampleRevitAddin")]
+        [assembly: AssemblyDescription("")]
+        [assembly: AssemblyConfiguration("")]
+        [assembly: AssemblyCompany("")]
+        [assembly: AssemblyProduct("SampleRevitAddin")]
+        [assembly: AssemblyTrademark("")]
+        [assembly: AssemblyCulture("")]
+        public const string Version = "0.1.0";
+    }
+}
